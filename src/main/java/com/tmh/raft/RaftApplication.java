dcad58ddf04d0cfe7bf100a73783834ebf07f48b
yuhao <hughieyu@163.com>
2019-12-24 13:11:18 +0800

    init commit

---
+++ src/main/java/com/tmh/raft/RaftApplication.java
@@ -0,0 +1,13 @@
+package com.tmh.raft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmh/raft/RaftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmh.raft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
