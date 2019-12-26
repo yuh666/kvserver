40ddbe252a68f5056cd9fe29912d6b30c1a59cf4
yuhao <hughieyu@163.com>
2019-12-26 10:48:20 +0800

    docker build

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 mvn -Dmaven.test.skip=true clean package
 mv target/kvserver-0.0.1-SNAPSHOT.jar .
 docker build -t kvserver:latest .
-docker-compose up -d
+docker-compose up
 docker rmi $(docker images -f "dangling=true" -q) > /dev/null 2>&1
