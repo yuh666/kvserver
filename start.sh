e9638e5042a50725e49da452195ce9805f22e22e
yuhao <hughieyu@163.com>
2019-12-29 12:10:09 +0800

    挂载日志目录

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mvn -Dmaven.test.skip=true clean package
-mv target/kvserver-0.0.1-SNAPSHOT.jar .
+cp target/kvserver-0.0.1-SNAPSHOT.jar .
 docker build -t kvserver:latest .
-docker-compose up
+rm -f kvserver-0.0.1-SNAPSHOT.jar
+docker-compose up -d
 docker rmi $(docker images -f "dangling=true" -q) > /dev/null 2>&1
